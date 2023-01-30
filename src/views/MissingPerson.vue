551121620beadcd206bbdc8cca2003862da60981
Ahmeddessoky <ahmeddessoky552001@gmail.com>
2023-01-30 20:28:57 +0200

    missing

--- src/views/MissingPerson.vue
+++ src/views/MissingPerson.vue
@@ -3,20 +3,20 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+// import { mapActions } from "vuex";
 import MissingPerson from "../components/MissingPerson/MissingPerson.vue";
 export default {
   name: "MissingPersonPage",
   components: { MissingPerson },
-  async mounted() {
-    let user = localStorage.getItem("user-Info");
-    if (!user) {
-      this.redirectTo({ val: "SignUp" });
-    }
-  },
-  methods: {
-    ...mapActions(["redirectTo"]),
-  },
+  // async mounted() {
+  //   let user = localStorage.getItem("user-Info");
+  //   if (!user) {
+  //     this.redirectTo({ val: "SignUp" });
+  //   }
+  // },
+  // methods: {
+  //   ...mapActions(["redirectTo"]),
+  // },
 };
 </script>
 
--- src/views/SearchMissing.vue
+++ src/views/SearchMissing.vue
@@ -3,23 +3,23 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+// import { mapActions } from "vuex";
 import SearchMissing from "../components/SearchForMissing/SearchForMissing.vue";
 export default {
   name: "SearchMissingPage",
   components: { SearchMissing },
-  async mounted() {
-    let user = localStorage.getItem("user-Info");
-    if (!user) {
-      this.redirectTo({ val: "SignUp" });
-    } else {
-      this.username = JSON.parse(user).name;
-      this.userId = JSON.parse(user).id;
-    }
-  },
-  methods: {
-    ...mapActions(["redirectTo"]),
-  },
+  // async mounted() {
+  //   let user = localStorage.getItem("user-Info");
+  //   if (!user) {
+  //     this.redirectTo({ val: "SignUp" });
+  //   } else {
+  //     this.username = JSON.parse(user).name;
+  //     this.userId = JSON.parse(user).id;
+  //   }
+  // },
+  // methods: {
+  //   ...mapActions(["redirectTo"]),
+  // },
 };
 </script>
 
