d3b1a4bccca8378ceffc489a935dce63cd3bd771
Ahmeddessoky <ahmeddessoky552001@gmail.com>
2023-04-25 11:28:56 +0200

    uplodae

--- src/views/ThePerson.vue
+++ src/views/ThePerson.vue
@@ -3,11 +3,22 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import ThePerson from "../components/ThePerson/ThePerson.vue";
 export default {
   name: "ThePersonPage",
   components: {
     ThePerson,
+  },
+  async mounted() {
+    let user = localStorage.getItem("user-Info");
+    let token = localStorage.getItem("token");
+    if (!user && !token) {
+      this.redirectTo({ val: "SignUp" });
+    }
+  },
+  methods: {
+    ...mapActions(["redirectTo"]),
   },
 };
 </script>
